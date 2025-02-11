1078cf1ab77508d1eaab4a5ecd450172d6eb4e0d
Adrian Garcia <adrian.garcia@acordeinternational.com>
2025-02-11 18:27:28 +0100

    Carga inicial del proyecto

---
+++ src/test/java/com/gapsi/test/springBootTest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gapsi.test.springBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
